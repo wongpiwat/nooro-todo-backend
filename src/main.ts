b1e35a8a01d4b745624d93b3600ae4754f3bba90
wongpiwat <wongpiwat.san@gmail.com>
2024-12-23 17:26:16 -0500

    setup prisma to the project

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT;
+
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/tasks", async (req, res) => {
+  const tasks = await prisma.task.findMany();
+  res.json(tasks);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`🚀 Server ready at: http://localhost:${port}`);
 });
