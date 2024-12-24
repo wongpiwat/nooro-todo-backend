d6e48f5f7ae4d49e12f6ec93bf26e1ecbb46a362
wongpiwat <wongpiwat.san@gmail.com>
2024-12-23 20:56:46 -0500

    enable cors for all routes on express

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import taskRoute from "./routes/task.route";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT;
